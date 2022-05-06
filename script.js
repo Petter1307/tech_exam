955baeda39a5aad4bf5fb93f5aa22a24e4850594
Petter1307 <petrealupu75@gmail.com>
2022-05-06 14:03:27 +0300

    Fixed bug with card

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function change_icon(){
-    x = document.getElementById("fav");
-    y = document.getElementById("not_fav");
-    if(window.getComputedStyle(x).display=="flex"){
-        y.style.display = "flex";
-        x.style.display = "none";
-        return;
-    }else if(window.getComputedStyle(y).display=="flex"){
-        x.style.display = "flex";
-        y.style.display = "none";
-        return;
-    }
-}
+function change_icon(x) {
+    x.classList.toggle("fa-solid");
+  }
